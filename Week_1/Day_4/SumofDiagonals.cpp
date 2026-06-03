ac9b4a6161b6f3c797901ac0485afeed7a55c254
Tushar Sharma <tustustefg@gmail.com>
2026-06-03 22:06:23 +0530

    started day 04

---
+++ Week_1/Day_4/SumofDiagonals.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + mat[i][i];
+            if (i != n - i - 1) {
+                sum += mat[i][n - 1 - i];
+            }
+        }
+
+        return sum;
+    }
+};
